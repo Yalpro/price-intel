f61bcaa4333e445eba87c76d8d38a3ebba449d2c
SMTA7 <thameemansarivrn@gmail.com>
2026-08-12 02:57:13 +0530

    fix: production vercel rewrites for /api/* backend proxy

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d8a842flao8"
+    "revision": "0.etc4au8mmm4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
